205a00667afdb46f505e73b3c5060ca6c3a9990d
Flere21 <aaronhans2134@gmail.com>
2023-05-13 19:52:19 +0800

    imported time module

--- test_tv.py
+++ test_tv.py
@@ -1,4 +1,5 @@
 from television import TV
+import time
 
 #creates tv 1
 television_1 = TV()
@@ -9,6 +10,7 @@ television_1.set_channel(30)
 #set volume of tv 1 to 3
 television_1.set_volume(3)
 #display tv 1 status
+time.sleep(1)
 print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 #increase volume up by 2
 television_1.volume_up()
@@ -16,6 +18,7 @@ television_1.volume_up()
 #increase channel by 1
 television_1.channel_up()
 #display tv status
+time.sleep(1)
 print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 #decrease volume by 1
 television_1.volume_down()
@@ -23,6 +26,7 @@ television_1.volume_down()
 television_1.channel_down()
 television_1.channel_down()
 #display tv status
+time.sleep(1)
 print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 
 #create tv 2
@@ -34,13 +38,16 @@ television_2.set_channel(3)
 #set volume of tv to 2
 television_2.set_volume(2)
 #display tv 2 status
+time.sleep(1)
 print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
 #increase channel and volume by 1
 television_2.channel_up()
 television_2.volume_up()
 #display tv status
+time.sleep(1)
 print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
 #decrease channel by 1
 television_2.channel_down()
 #display tv status
+time.sleep(1)
 print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
