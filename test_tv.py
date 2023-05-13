f6db75a97d17acd89d86e44a5bd8cc6b6e1f095a
Flere21 <aaronhans2134@gmail.com>
2023-05-13 20:06:22 +0800

    added colorama

--- test_tv.py
+++ test_tv.py
@@ -1,5 +1,8 @@
 from television import TV
 import time
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 #creates tv 1
 television_1 = TV()
@@ -11,7 +14,7 @@ television_1.set_channel(30)
 television_1.set_volume(3)
 #display tv 1 status
 time.sleep(1)
-print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
+print(Fore.GREEN + Style.BRIGHT + '\nTelevision 1 is on channel ' + str(television_1.get_channel()) + ' and volume is at '+ str(television_1.get_volume()))
 #increase volume up by 2
 television_1.volume_up()
 television_1.volume_up()
@@ -19,7 +22,7 @@ television_1.volume_up()
 television_1.channel_up()
 #display tv status
 time.sleep(1)
-print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
+print(Fore.YELLOW + Style.BRIGHT + '\nTelevision 1 is on channel ' + str(television_1.get_channel()) + ' and volume is at '+ str(television_1.get_volume()))
 #decrease volume by 1
 television_1.volume_down()
 #decrease channel by 2
@@ -27,7 +30,7 @@ television_1.channel_down()
 television_1.channel_down()
 #display tv status
 time.sleep(1)
-print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
+print(Fore.BLUE + Style.BRIGHT + '\nTelevision 1 is on channel ' + str(television_1.get_channel()) + ' and volume is at '+ str(television_1.get_volume()))
 
 #create tv 2
 television_2 = TV()
@@ -39,15 +42,14 @@ television_2.set_channel(3)
 television_2.set_volume(2)
 #display tv 2 status
 time.sleep(1)
-print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
+print(Fore.GREEN + Style.BRIGHT + '\nTelevision 2 is on channel ' + str(television_2.get_channel()) + ' and volume is at '+ str(television_2.get_volume()))
 #increase channel and volume by 1
 television_2.channel_up()
 television_2.volume_up()
 #display tv status
 time.sleep(1)
-print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
-#decrease channel by 1
+print(Fore.YELLOW + Style.BRIGHT + '\nTelevision 2 is on channel ' + str(television_2.get_channel()) + ' and volume is at '+ str(television_2.get_volume()))#decrease channel by 1
 television_2.channel_down()
 #display tv status
 time.sleep(1)
-print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
+print(Fore.BLUE + Style.BRIGHT + '\nTelevision 2 is on channel ' + str(television_2.get_channel()) + ' and volume is at '+ str(television_2.get_volume()))
