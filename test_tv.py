43666ee0880939f45327f8fc1b3ea6a8ecb8f589
Flere21 <aaronhans2134@gmail.com>
2023-05-13 19:50:01 +0800

    added status changes in tv 2 as well

--- test_tv.py
+++ test_tv.py
@@ -9,19 +9,21 @@ television_1.set_channel(30)
 #set volume of tv 1 to 3
 television_1.set_volume(3)
 #display tv 1 status
-print('Television 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
+print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 #increase volume up by 2
 television_1.volume_up()
 television_1.volume_up()
 #increase channel by 1
 television_1.channel_up()
-print('Television 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
+#display tv status
+print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 #decrease volume by 1
 television_1.volume_down()
 #decrease channel by 2
 television_1.channel_down()
 television_1.channel_down()
-print('Television 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
+#display tv status
+print('\nTelevision 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 
 #create tv 2
 television_2 = TV()
@@ -32,4 +34,13 @@ television_2.set_channel(3)
 #set volume of tv to 2
 television_2.set_volume(2)
 #display tv 2 status
-print('Television 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
+print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
+#increase channel and volume by 1
+television_2.channel_up()
+television_2.volume_up()
+#display tv status
+print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
+#decrease channel by 1
+television_2.channel_down()
+#display tv status
+print('\nTelevision 2 is on channel', television_2.get_channel(), 'and volume is at', television_2.get_volume())
