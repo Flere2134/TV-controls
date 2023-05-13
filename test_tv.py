6fc05c1dbf3867e89b5d50a642afa4b564aa192a
Flere21 <aaronhans2134@gmail.com>
2023-05-13 19:22:08 +0800

    finished setting up tv 1

--- test_tv.py
+++ test_tv.py
@@ -1,10 +1,15 @@
 from television import TV
 
 #creates tv 1
+television_1 = TV()
 #turn on tv 1
+television_1.turn_on()
 #set channel of tv 1 to 30
+television_1.set_channel(30)
 #set volume of tv 1 to 3
+television_1.set_volume(3)
 #display tv 1 status
+print('Television 1 is on channel', television_1.get_channel(), 'and volume is at', television_1.get_volume())
 
 #create tv 2
 #turn on tv2
