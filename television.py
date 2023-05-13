231a8958a4f03d5412624cad0dc21caf6232d52b
Flere21 <aaronhans2134@gmail.com>
2023-05-13 18:41:58 +0800

    listed the methods needed to create

--- television.py
+++ television.py
@@ -1,5 +1,16 @@
 class TV:
-    def __init__(self, channel, volume_level, power)
+    #constructor
+    def __init__(self, channel, volume_level, power):
         self.channel = channel
         self.volume_level = volume_level
         self.power = power
+    #method to turn on tv
+    #method to turn off tv
+    #method to get channel of tv
+    #method to set channel of tv
+    #method to get volume of tv
+    #method to set volume of tv
+    #method to channel up tv
+    #method to channel down tv
+    #method to volume up tv
+    #method to volume down tv
