c3cff4103854590e512fc4181ea5ef3b45e11cbf
Flere21 <aaronhans2134@gmail.com>
2023-05-13 19:18:15 +0800

    imported class TV and created pseudocode setting up the tv

---
+++ test_tv.py
@@ -0,0 +1,13 @@
+from television import TV
+
+#creates tv 1
+#turn on tv 1
+#set channel of tv 1 to 30
+#set volume of tv 1 to 3
+#display tv 1 status
+
+#create tv 2
+#turn on tv2
+#set channel of tv 2 to 3
+#set volume of tv to 2
+#display tv 2 status
